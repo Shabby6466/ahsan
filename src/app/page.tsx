4fa3b4640ce6d8fbd99417ca6d011e49b9f89d64
ahsnkhn <ahsankhan23125@gmail.com>
2025-08-31 19:07:22 +0500

    updated size from pixels to percentages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,19 +26,19 @@ return (
                 create an account
               </a>
             </p>
-            <div className="w-full h-px bg-gray-600 my-4"></div>
+            <div className="w-[94%] h-px bg-gray-600 my-4"></div>
           </div>
           {/* input */}
           {/* email and password */}
           <div className="flex flex-col justify-start gap-3 px-4">
             <input
-              className="border-2 border-gray-500 w-[300px] px-2 py-2 rounded-xl bg-white shadow-md"
+              className="border-2 border-gray-500 w-[94%] px-2 py-2 rounded-xl bg-white shadow-md"
               placeholder="Email"
               type="email"
             ></input>
 
             <input
-              className="border-2 border-gray-500 w-[300px] px-2 py-2 rounded-xl bg-white shadow-md"
+              className="border-2 border-gray-500 w-[94%] px-2 py-2 rounded-xl bg-white shadow-md"
               placeholder="Password"
               type="password"
             ></input>
@@ -53,12 +53,12 @@ return (
           </div>
           {/* buttons */}
           <div className="pl-4 flex flex-col gap-2 justify-start mb-2">
-            <button className="bg-blue-500 text-white w-[300px] px-2 py-2 rounded-2xl font-semibold hover:bg-blue-600 active:scale-97 transition-all">
+            <button className="bg-blue-500 text-white w-[91%] px-2 py-2 rounded-2xl font-semibold hover:bg-blue-600 active:scale-97 transition-all">
               {" "}
               Sign in{" "}
             </button>
 
-            <button className="border border-black-500 w-[300px] px-2 py-2 flex items-center justify-center gap-2 rounded-2xl font-semibold hover:bg-gray-200 active:scale-97 transition-all">
+            <button className="border border-black-500 w-[91%] px-2 py-2 flex items-center justify-center gap-2 rounded-2xl font-semibold hover:bg-gray-200 active:scale-97 transition-all">
               <img src="/google.png" alt="google" className="w-6 h-6" />
               Google
             </button>
