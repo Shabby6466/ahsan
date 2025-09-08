e1315327c059e7a62fc27c3aa351473ec8bd88b9
ahsnkhn <ahsankhan23125@gmail.com>
2025-09-08 15:55:02 +0500

    fixed header, logo and updated names

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,30 +1,31 @@
-import styles from "@/styles/header.module.css";
+import styles from "./styles/header.module.css";
 
 export default function Header() {
   return (
     <div>
       <header>
-        {/* header */}
-        <div className="{styles.Header} px-8 py-5 flex items-center justify-between shadow-md">
+        <div className = " w-screen border border-gray-800 flex items-center justify-between">
+        {/* Corrected className using a template literal */}
+        <div className={`${styles.Header} pt-[4px] pb-[4px] pr-[20px] pl-[20px] w-[100%] shadow-md rounded-2xl`}>
           {/* outermost div */}
-          <div className="flex space-x-[5px] items-center justify-left w-[200px]">
-            {/* CM logo and text div */}
-            <div className="">
-              <img src="/logo.svg" alt="red" className="w-[60px] h-[70px]" />
+          <div className="flex space-x-[5px] items-center justify-left w-[900px]">
+            {/* CFCX logo div */}
+            <div className="flex items-center justify-left">
+              <img src="/cfcx.svg" alt="red" className="w-[60px] h-[70px]" />
             </div>
-            <div className="">
-              <h4 className="text-white font-stretch-ultra-expanded"> Lelota</h4>
-              <h4 className="text-yellow-300 font-bold">Marketing</h4>
+            <div className = "flex flex-col items-center justify-between "> 
+              {/* made a div for the text to be centered*/}
+              <div className="">
+              <h6>Swap</h6>
+              <h6>Copy Trading</h6>
+              <h6>Portifolio</h6>
+              <h6>Referral</h6>
+            </div>
             </div>
           </div>
+          </div>
           <div>
-            <div className="flex flex-row items-center justify-between w-[900px]">
-              <h6>Home</h6>
-              <h6>Services</h6>
-              <h6>Blog</h6>
-              <h6>About</h6>
-              <h6>FAQ</h6>
-            </div>
+            
           </div>
         </div>
       </header>
